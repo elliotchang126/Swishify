8ae5227e27c8c2f1669548051eaa9ddf1ddf92c7
Elliot Chang <elliotchang126@gmail.com>
2023-07-26 16:22:31 -0700

    test

--- src/scripts/profile.js
+++ src/scripts/profile.js
@@ -5,7 +5,7 @@ export function writeProfile(player) {
 
     const img = d3.select(".profile-image")
         .append("img")
-        .attr('src', `/assets/player_headshots/${player}.webp`)
+        .attr('src', `${window.location}/assets/player_headshots/${player}.webp`)
         .attr("width", 300);
 
 
